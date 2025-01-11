1e5c6ce818d480b4ba97ac4e3a0e5d6a6a37ec7d
azuraopal <muhammadnovals334@gmail.com>
2025-01-11 15:59:54 +0700

    perf(Routes) Add routes for pembelian

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\PembelianController;
 use App\Http\Controllers\Api\PenjualanController;
 use App\Http\Controllers\Api\ProdukController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('auth')->group(function () {
+    
     Route::prefix('users')->group(function () {
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
@@ -16,8 +19,10 @@ Route::prefix('auth')->group(function () {
         Route::get('/verify-email/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
         Route::get('produk/print-pdf', [ProdukController::class, 'printPdf']);
         Route::get('penjualan/print-pdf', [PenjualanController::class, 'printPdf']);
+        Route::get('pembelian/print-pdf', [PembelianController::class, 'printPdf']);
 
         Route::middleware('auth:sanctum')->group(function () {
+
             // Routes Auth
             Route::post('/logout', [AuthController::class, 'logout']);
             Route::post('/update-profile-picture', [AuthController::class, 'updateProfilePicture']);
@@ -25,6 +30,13 @@ Route::prefix('auth')->group(function () {
             Route::post('/reset-password-v2', [AuthController::class, 'resetPasswordV2'])
                 ->middleware(['password.verified']);
             Route::get('/profile', [AuthController::class, 'getProfile']);
+
+            // Routes Statistik Dashboard
+            Route::get('/laba-bulanan', action: [DashboardController::class, 'getLabaBersihBulanan']);
+            Route::get('/laba-tahunan', [DashboardController::class, 'getLabaBersihTahunan']);
+            Route::get('/total-produk', [DashboardController::class, 'getTotalProduk']);
+            Route::get('/total-terjual', [DashboardController::class, 'getTotalProdukTerjual']);
+            Route::get('/all', [DashboardController::class, 'getSemuaStatistik']);
 
             // Routes kategori
             Route::get('kategori', [KategoriController::class, 'index']);
@@ -42,6 +54,9 @@ Route::prefix('auth')->group(function () {
             // Routes Penjualan
             Route::get('penjualan', [PenjualanController::class, 'index']);
             Route::post('penjualan', [PenjualanController::class, 'store']);
+
+            // Routes Pembelian
+            Route::get('pembelian', [PembelianController::class, 'index']);
         });
     });
 });
