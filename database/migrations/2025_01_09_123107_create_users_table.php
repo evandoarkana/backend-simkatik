2c27c6f22f03b1cbe9e46d8c3eeefa1c5ba10cc0
azuraopal <muhammadnovals334@gmail.com>
2025-02-15 22:11:59 +0700

    Refactor(users_table): Add 'nama_lengkap' and 'role' fields in create_users_table migration

--- database/migrations/2025_01_09_123107_create_users_table.php
+++ database/migrations/2025_01_09_123107_create_users_table.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Enums\UserRole;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_lengkap');
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('profile_picture')->default('default.jpg');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', UserRole::values())->default(UserRole::Karyawan->value);
             $table->rememberToken();
             $table->timestamps();
         });
